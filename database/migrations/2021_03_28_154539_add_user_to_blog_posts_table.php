5736786f46f90d5136304dc6e0c1423362f84e0b
Alex van Niekerk <vanniekerk.alex@gmail.com>
2021-03-28 20:54:51 +0200

    Updated the phpunit.xml file, from server to env

--- database/migrations/2021_03_28_154539_add_user_to_blog_posts_table.php
+++ database/migrations/2021_03_28_154539_add_user_to_blog_posts_table.php
@@ -14,7 +14,13 @@ class AddUserToBlogPostsTable extends Migration
 		public function up()
 		{
 				Schema::table('blog_posts', function (Blueprint $table) {
-						$table->unsignedInteger('user_id');
+						// $table->unsignedBigInteger('user_id');
+
+						if (env('DB_CONNECTION') === 'sqlite_testing') {
+							$table->unsignedBigInteger('user_id')->default(0);
+						} else {
+							$table->unsignedBigInteger('user_id');
+						}
 
 						$table->foreign('user_id')
 							->references('id')->on('users');
